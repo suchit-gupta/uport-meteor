4fd9d76a4bf37b98944b292aece588cb2bbe8793
Zach Ferland <zachferland@gmail.com>
2017-02-15 16:37:08 -0500

    add minimal meteor app, uport starts onload

---
+++ README.md
@@ -0,0 +1,13 @@
+
+Need meteor?
+```
+curl https://install.meteor.com/ | sh
+```
+
+```
+$ git clone https://github.com/uport-project/uport-meteor.git
+$ cd uport-meteor
+$ meteor npm install
+$ rm -rf node_modules/crypto
+$ meteor
+```
---
+++ client/main.js
@@ -0,0 +1,27 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Connect } from 'uport-connect'
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+  // counter starts at 0
+  this.counter = new ReactiveVar(0);
+  this.uport = new Connect('MyApp');
+  this.uport.requestCredentials().then((credentials) => {
+   console.log(credentials)
+  });
+});
+
+Template.hello.helpers({
+  counter() {
+    return Template.instance().counter.get();
+  },
+});
+
+Template.hello.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.counter.set(instance.counter.get() + 1);
+  },
+});
